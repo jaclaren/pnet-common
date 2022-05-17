93d33e9a212d8fa9d59f9dbefebbf015365b0bc4
Jani <jani.claren@gmail.com>
2022-05-17 13:19:24 +0200

    Development

--- dist/dev.d.ts
+++ dist/dev.d.ts
@@ -1,1 +1,2 @@
+declare const generateDetails: (item: any) => HTMLDivElement;
 declare const generateElements: (items: any) => void;
--- dist/dev.js
+++ dist/dev.js
@@ -1,12 +1,30 @@
 "use strict";
+const generateDetails = (item) => {
+    const element = document.createElement(`div`);
+    const title = document.createElement(`div`);
+    element.classList.add(`pnet-eclc-details`);
+    title.classList.add(`pnet-eclc-title`);
+    title.innerHTML = item.title;
+    element.append(title);
+    return element;
+};
 const generateElements = (items) => {
     const root = document.createElement(`div`);
     root.innerHTML = "Hellos";
-    document.querySelectorAll('.tmp').forEach(tmp => {
-        items.body.games.forEach((item) => {
-            const div = document.createElement(`div`);
-            div.innerHTML = `${item.title}`;
-            tmp.append(div);
+    document.querySelectorAll('.pnet-expcardlist').forEach(tmp => {
+        items.body.games.forEach((item, index) => {
+            const root = document.createElement(`div`);
+            const imgCol = document.createElement(`div`);
+            root.classList.add(`pnet-ecl-card`);
+            imgCol.classList.add(`pnet-eclc-img`);
+            imgCol.style.background = `url(${item.coverimage})`;
+            const title = document.createElement(`div`);
+            title.classList.add(`pnet-eclc-card-title`);
+            root.append(imgCol);
+            if (index == 6)
+                root.append(generateDetails(item));
+            root.addEventListener('click', () => root.append(generateDetails(item)));
+            tmp.append(root);
         });
     });
 };
