6f07c0cbea146b677a51289c80cdfa9dcbfa0771
Jani <jani.claren@gmail.com>
2022-05-17 16:33:31 +0200

    Styling

--- dist/dev.js
+++ dist/dev.js
@@ -2,10 +2,25 @@
 const generateDetails = (item) => {
     const element = document.createElement(`div`);
     const title = document.createElement(`div`);
+    const reviewCount = document.createElement(`div`);
+    const score = document.createElement(`div`);
+    const bg = document.createElement(`div`);
+    const button = document.createElement(`button`);
     element.classList.add(`pnet-eclc-details`);
+    bg.classList.add(`pnet-eclc-bg`);
     title.classList.add(`pnet-eclc-title`);
+    reviewCount.classList.add(`pnet-eclc-reviewCount`);
+    score.classList.add(`pnet-eclc-score`);
     title.innerHTML = item.title;
+    reviewCount.innerHTML = item.reviewCount;
+    score.innerHTML = item.average_score;
+    button.textContent = `Kooste`;
+    bg.style.background = `url(${item.coverimage})`;
+    element.append(bg);
     element.append(title);
+    element.append(reviewCount);
+    element.append(score);
+    element.append(button);
     return element;
 };
 const generateElements = (items) => {
--- src/dev.ts
+++ src/dev.ts
@@ -1,13 +1,29 @@
 const generateDetails = (item:any) => {
     const element = document.createElement(`div`)
     const title = document.createElement(`div`)
+    const reviewCount = document.createElement(`div`)
+    const score = document.createElement(`div`)
+    const bg = document.createElement(`div`)
+    const button = document.createElement(`button`)    
 
     element.classList.add(`pnet-eclc-details`)    
+    bg.classList.add(`pnet-eclc-bg`)
     title.classList.add(`pnet-eclc-title`)
+    reviewCount.classList.add(`pnet-eclc-reviewCount`)
+    score.classList.add(`pnet-eclc-score`)
 
     title.innerHTML = item.title
+    reviewCount.innerHTML = item.reviewCount
+    score.innerHTML = item.average_score
+    button.textContent = `Kooste`
+
+    bg.style.background = `url(${item.coverimage})`
     
+    element.append(bg)
     element.append(title)
+    element.append(reviewCount)
+    element.append(score)
+    element.append(button)    
 
     return element
 }
@@ -35,7 +51,6 @@ const generateElements = (items: any) => {
                 root.append(generateDetails(item))
                 
             root.addEventListener('click', () => root.append(generateDetails(item)))
-
             
             tmp.append(root)                    
         }) 
